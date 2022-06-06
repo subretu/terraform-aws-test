abe518127f34c8dbdf0a9e80141b69d14018230f
miyahara <50367145+subretu@users.noreply.github.com>
2022-06-06 23:50:28 +0900

    変数の定義を変更

--- modules/common/provider.tf
+++ modules/common/provider.tf
@@ -2,7 +2,7 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
   region = "ap-northeast-1"
 }
