13095d55f99528c264f6bc41f783bf28d6d676a1
miyahara <50367145+subretu@users.noreply.github.com>
2022-06-09 23:29:42 +0900

    変数を定義

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,7 +1,3 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-
-
 terraform {
   required_version = ">= 0.12.28"
   required_providers {
@@ -11,10 +7,10 @@ terraform {
 
 module "provider" {
   source = "./modules/provider"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 module "vpc" {
   source = "./modules/vpc"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
 }
