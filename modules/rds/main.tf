b352654117bd0c144afbc072b6fdb6aedb8250d7
miyahara <50367145+subretu@users.noreply.github.com>
2022-06-12 22:33:04 +0900

    不要な定義を削除

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -12,7 +12,6 @@ resource "aws_db_instance" "rds" {
   username               = var.db_username
   password               = var.db_password
   skip_final_snapshot    = true
-  vpc_security_group_ids = [aws_security_group.rds-sg.id]
   db_subnet_group_name   = aws_db_subnet_group.rds-subnet-group.name
 }
 
