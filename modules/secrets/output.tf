c4012ba32c7c24ff8251d0e73348a902e30fa2fa
miyahara <50367145+subretu@users.noreply.github.com>
2022-06-14 23:10:07 +0900

    outputを修正

--- modules/secrets/output.tf
+++ modules/secrets/output.tf
@@ -1,1 +1,3 @@
-output "db_password" { value = data.aws_secretsmanager_secret_version.db-password.secret_string }
+//output "db_password" { value = data.aws_secretsmanager_secret_version.db-password.secret_string }
+
+output "db_password" { value = aws_secretsmanager_secret_version.db-password.secret_string }
