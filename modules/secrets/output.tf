367a56cdbe6eca9ab70f5de3cae2401135754ae7
miyahara <50367145+subretu@users.noreply.github.com>
2022-06-14 23:37:21 +0900

    data sourceに戻す

--- modules/secrets/output.tf
+++ modules/secrets/output.tf
@@ -1,3 +1,1 @@
-//output "db_password" { value = data.aws_secretsmanager_secret_version.db-password.secret_string }
-
-output "db_password" { value = aws_secretsmanager_secret_version.db-password.secret_string }
+output "db_password" { value = data.aws_secretsmanager_secret_version.db-password.secret_string }
