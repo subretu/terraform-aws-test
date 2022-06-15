8edf1bd03a2b2d966e9355c2af12769bd28c3c5f
miyahara <50367145+subretu@users.noreply.github.com>
2022-06-15 23:01:35 +0900

    data sourceの削除

--- modules/secrets/main.tf
+++ modules/secrets/main.tf
@@ -15,15 +15,5 @@ resource "aws_secretsmanager_secret" "db-password" {
 resource "aws_secretsmanager_secret_version" "db-password" {
   secret_id     = aws_secretsmanager_secret.db-password.id
   secret_string = random_password.db-password.result
+  sensitive     = true
 }
-
-data "aws_secretsmanager_secret" "db-password" {
-  name       = "${var.name}-db-password"
-  depends_on = [aws_secretsmanager_secret.db-password]
-}
-
-data "aws_secretsmanager_secret_version" "db-password" {
-  secret_id  = data.aws_secretsmanager_secret.db-password.id
-  depends_on = [aws_secretsmanager_secret_version.db-password]
-}
-
