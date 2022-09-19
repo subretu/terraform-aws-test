ea7f1bf0155a555714778b4583e26eb5f219d650
miyahara <50367145+subretu@users.noreply.github.com>
2022-09-19 23:43:03 +0900

    test

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -32,7 +32,7 @@ resource "aws_subnet" "private-1" {
 
 resource "aws_subnet" "private-2" {
   vpc_id            = aws_vpc.test-vpc.id
-  cidr_block        = "10.0.3.0/24"
+  cidr_block        = "10.0.4.0/24"
   availability_zone = "ap-northeast-1c"
   tags = {
     Name = "private-2"
