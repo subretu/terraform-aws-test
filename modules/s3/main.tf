6bc6f11911eea16cde8d728398db2ac829adaeb5
miyahara <50367145+subretu@users.noreply.github.com>
2023-03-12 21:49:28 +0900

    fix

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,13 +1,13 @@
-resource "aws_s3_bucket" "subretu_vue_webhost" {
-  bucket = "subretu_vue_webhost"
+resource "aws_s3_bucket" "subretu-vue-webhost" {
+  bucket = "subretu-vue-webhost"
 }
 
-resource "aws_s3_bucket_policy" "subretu_vue_webhost_allow_access" {
-  bucket = aws_s3_bucket.subretu_vue_webhost.id
-  policy = data.aws_iam_policy_document.subretu_vue_webhost_allow_access.json
+resource "aws_s3_bucket_policy" "subretu-vue-webhost_allow_access" {
+  bucket = aws_s3_bucket.subretu-vue-webhost.id
+  policy = data.aws_iam_policy_document.subretu-vue-webhost_allow_access.json
 }
 
-data "aws_iam_policy_document" "subretu_vue_webhost_allow_access" {
+data "aws_iam_policy_document" "subretu-vue-webhost_allow_access" {
   statement {
     actions = ["s3:GetObject"]
     effect  = "Allow"
@@ -15,18 +15,18 @@ data "aws_iam_policy_document" "subretu_vue_webhost_allow_access" {
       type        = "AWS"
       identifiers = ["*"]
     }
-    resources = ["arn:aws:s3:::subretu_vue_webhost/*"]
+    resources = ["arn:aws:s3:::subretu-vue-webhost/*"]
     sid       = "PublicReadGetObject"
   }
 }
 
-resource "aws_s3_bucket_acl" "subretu_vue_webhost_acl" {
-  bucket = aws_s3_bucket.subretu_vue_webhost.id
+resource "aws_s3_bucket_acl" "subretu-vue-webhost_acl" {
+  bucket = aws_s3_bucket.subretu-vue-webhost.id
   acl    = "public-read"
 }
 
-resource "aws_s3_bucket_website_configuration" "subretu_vue_webhost_website_config" {
-  bucket = aws_s3_bucket.subretu_vue_webhost.id
+resource "aws_s3_bucket_website_configuration" "subretu-vue-webhost_website_config" {
+  bucket = aws_s3_bucket.subretu-vue-webhost.id
 
   index_document {
     suffix = "index.html"
